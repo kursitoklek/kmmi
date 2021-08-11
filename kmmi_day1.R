e901fe40d8ce63259ee309343485be5c6e925969
kursitoklek <saputra.langgang.nara@gmail.com>
2021-08-11 11:16:46 +0700

    kmmiday1

---
+++ kmmi_day1.R
@@ -0,0 +1,32 @@
+#shortcut untuk buat script bru adalah ctrl+ shift + n
+#untuk save dengan ctrl + s, yakalipoake yang lain
+#dasar2 R
+
+# tanda pagar digunkana untuk membuat sebuha komentar
+1 + 1
+6/2
+10 %% 2
+
+#membuat obvject bisa menggunakan = atau assignt denganm <-
+d1 = 1+1
+d2 =2+3
+d3 <- 6/2
+d4 <- 10%%3
+d5 = 1
+d5 + d3
+
+#kita hanya bisa analisi data jika data sudah di run atau import di environment bruh
+
+#tanda lainnya
+1 < 2 #kurang
+1> 2 #lebih
+ 10 <= 6 #kurang samadengan
+ 2 >= 2 #lebih samadengan
+ 
+10 == 10 #sama persis
+
+"aku" == "kamu"
+
+"aku" != "kamu" #tidak sama dengan
+#isTRUE cek apakah benar
+#isFALSE cek apakah salah
